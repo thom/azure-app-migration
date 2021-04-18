9046fec3a8aca82cc24812682f1a144ed89ae47e
Thomas Weibel <thom@ikhono.net>
2021-04-18 22:58:27 +0200

    Update ServiceBusQueueTrigger
    Fix SendGrid and SQL code

--- function/ServiceBusQueueTrigger/__init__.py
+++ function/ServiceBusQueueTrigger/__init__.py
@@ -4,14 +4,14 @@ import psycopg2
 import os
 from datetime import datetime
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import *
 
 
 def main(msg: func.ServiceBusMessage):
 
     notification_id = int(msg.get_body().decode('utf-8'))
     logging.info(
-        f"Python ServiceBus queue trigger processed message: {notification_id}")
+        f"Python ServiceBus queue trigger processed message: {str(notification_id)}")
 
     # Get connection to database
     conn = psycopg2.connect(
@@ -20,24 +20,27 @@ def main(msg: func.ServiceBusMessage):
         user=os.environ["POSTGRES_USER"],
         password=os.environ["POSTGRES_PW"]
     )
-    logging.info(f"Successfully connected to {POSTGRES_DB}@{POSTGRES_URL}")
+    logging.info(f"Successfully connected to database")
 
     try:
         # Get notification message and subject from database using the notification_id
         cur = conn.cursor()
-        cmd = f"SELECT message, subject FROM notification WHERE id={notification_id}"
+        cmd = f"SELECT message, subject FROM notification WHERE id={str(notification_id)}"
         cur.execute(cmd)
         logging.info(
-            f"Notification ID {notification_id}: Get message and subject")
+            f"Notification ID {str(notification_id)}: Get message and subject")
 
         for row in cur.fetchall():
             message = row[0]
             subject = row[1]
 
         if not message or not subject:
-            error_message = f"Notification ID {notification_id}: No message or subject"
+            error_message = f"Notification ID {str(notification_id)}: No message or subject"
             logging.error(error_message)
             raise Exception(error_message)
+
+        logging.info(
+            f"Notification ID {str(notification_id)}: Message '{message}', Subject '{subject}'")
 
         # Get attendees email and name
         cmd = f"SELECT first_name, last_name, email FROM attendee"
@@ -50,31 +53,32 @@ def main(msg: func.ServiceBusMessage):
             last_name = row[1]
             email = row[2]
 
-            message = Mail(
-                from_email=os.environ['ADMIN_EMAIL_ADDRESS'],
-                to_emails=email,
-                subject=f"Hello, {first_name}! {subject}",
-                plain_text_content=message
-            )
+            logging.info(
+                f"Notification ID {str(notification_id)}: First name '{first_name}', last name '{last_name}', email '{email}'")
 
+            from_email = Email(os.environ['ADMIN_EMAIL_ADDRESS'])
+            to_emails = To(email)
+            personalized_subject = f"Hello, {first_name}! {subject}"
+            content = Content("text/plain", message)
+
+            mail = Mail(from_email, to_emails, personalized_subject, content)
             sg = SendGridAPIClient(os.environ['SENDGRID_API_KEY'])
-            sg.send(message)
+            sg.send(mail)
 
             count += 1
 
-        # Update the notification table by setting the completed date and 
+        # Update the notification table by setting the completed date and
         # updating the status with the total number of attendees notified
         status = f"Notified {str(count)} attendees"
         date = datetime.now()
-        logging.info(f"Notification ID {notification_id}: {status}@{date}")
+        logging.info(f"Notification ID {str(notification_id)}: {status}@{date}")
 
-        cmd = f"UPDATE notification SET status = {status} WHERE id = {notification_id}"
+        cmd = f"UPDATE notification SET status='{status}' WHERE id={str(notification_id)}"
         cur.execute(cmd)
 
-        cmd = f"UPDATE notification SET completed_date = {date} WHERE id = {notification_id}"
+        cmd = f"UPDATE notification SET completed_date='{str(date)}' WHERE id={str(notification_id)}"
         cur.execute(cmd)
         conn.commit()
-
     except (Exception, psycopg2.DatabaseError) as error:
         logging.error(error)
     finally:
